e8ff8f372ead6e293709f126854b30ee33d1d792
Ana Paula <anapfnascimento@hotmail.com>
2019-05-29 15:45:32 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,13 @@
+package chess;
+
+public class ChessException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ChessException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ChessException(String msg) {
+		super(msg);
+	}
+}
